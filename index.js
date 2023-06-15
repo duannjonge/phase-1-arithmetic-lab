2a3ab62cd7ba6af0659dd07a0483760202336907
duan spot <brandennjonge@gmail.com>
2023-06-15 12:32:07 +0300

    did calculations

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+const num1=62;
+const num2=1;
+const multiply=num1*num2;
+multiply;
+
+const random= Math.floor(Math.random()*10)+1;
+
+const num3=14;
+const num4=10;
+const mod =num3%num4;
+mod;
+
+const max=Math.max(9,12, 14, 17, 18, 20);
+      max;
+    
